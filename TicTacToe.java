b59b9afee58a2297f40c734f9d0b8b257e611ddc
Akash Das <akashd595@gmail.com>
2022-05-26 11:59:18 +0530

    ADDED: uc5

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,22 +1,29 @@
 package com.bridgelabz.tictactoe;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
     static char[] arr = new char[10];
     static char user ;
     static char computer;
+    static int counter=0;
 
 
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Welocome to Tic-tac-Toe Masters");
         uc1();
-        System.out.println("Please enter your symbol ,X - O");
         uc2();
-        uc3();
-        uc4();
+       // uc3();
+        while(counter<10) {
+            if (counter % 2 == 0) {
+                uc4();
+            } else {
+                uc5();
+            }
+            counter++;
+        }
 
     }
     public static void uc1(){
@@ -27,9 +34,9 @@ public class TicTacToe {
     }
     public static void uc2(){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your symbol ,X - O");
         user = sc.next().charAt(0);
-
-        if (user == 'X'){
+        if (user == 'X' || user == 'x'){
             System.out.println("User choose X");
             System.out.println("Computer have O");
             computer = 'O';
@@ -40,30 +47,53 @@ public class TicTacToe {
         }
     }
     public static void uc3(){
+        System.out.println();
         for (int i=1;i<4;i++){
             System.out.print(arr[i]+" | ");
         }
         System.out.println();
-        for (int i=5;i<8;i++){
+        for (int i=4;i<7;i++){
             System.out.print(arr[i]+" | ");
         }
         System.out.println();
         for (int i=7;i<10;i++){
             System.out.print(arr[i]+" | ");
-
         }
-        System.out.print("  | ");
     }
     public static void uc4(){
         Scanner sc = new Scanner(System.in);
         System.out.println();
-        System.out.println("Choose the position (1-9)");
-        int pos = sc.nextInt();
-        if(arr[pos] == ' ') {
-            arr[pos] = user;
+        if(counter < 10){
+            System.out.println("please choose the position (1-9)");
+            int pos = sc.nextInt();
+
+            if(arr[pos] == ' ') {
+                arr[pos] = user;
+                //counter++;
+            }else{
+                System.out.println("Entered position is not empty please try again");
+                uc4();
+            }
         }else{
-            System.out.println("Entered position is empty please try again");
-            uc4();
+            System.out.println("No more free space");
+        }
+        uc3();
+    }
+    public static void uc5(){
+        Random random = new Random();
+        System.out.println();
+        if(counter<9){
+            System.out.println("computer's turn");
+
+            int pos = random.nextInt(9)+1;
+            if(arr[pos] == ' ') {
+                arr[pos] = computer;
+                //counter++;
+            }else{
+                uc5();
+            }
+        }else{
+            System.out.println("no more free space");
         }
         uc3();
     }
